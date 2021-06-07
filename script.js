5de51d46317a199c2d61a0b6a07756b516d736a5
Raj <33152218+Raj14619@users.noreply.github.com>
2021-06-08 00:50:57 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+    //PART 1 DECLARING VARIABLES
+/*
+    let js = "amazing"; // DECLARING A VARIABLE CALLED 'js' AS STRING
+    console.log(40 + 8 + 23 - 10); //OUTPUTTING CALCULATIONS TO CONSOLE
+
+    console.log("Jonas"); //OUTPUTTING TO CONSOLE
+    console.log(23); //OUTPUTTING TO CONSOLE
+
+    let firstName ="Jonas";
+    console.log(firstName);
+
+    let age = 23;// THIS IS STORED USING THE NUMBER DATA TYPE IN JS (WHICH STORES INTEGERS AND FLOATS)
+    let firstName = "Raj" // THIS IS THE STRING DATA TYPE IN JS
+    let checkbox = true // THIS IS THE BOOLEAN DATA TYPE IN JS
+    let undefinedVariable; // A variable which is declared and has an 'empty value'
+    let ok = null; // Null
+*/
+
+//PART 2 - USING TYPEOF TO FIGURE OUT DATA TYPE OF VARIABLES
+
+/*
+    console.log(typeof 23); // Number
+    console.log(typeof "Raj"); // String
+
+    let javscriptIsFun = true; //javascriptIsFun is a boolean
+    console.log(typeof javascriptIsFun); // javascriptIsFun is a boolean
+    javscriptIsFun = "YES!"; // javascriptIsFun is now a string
+    console.log(typeof javascriptIsFun) // this will now output string
+
+    let year;
+    console.log(year); // will be undefined because no data type and no value has been given
+    console.log(typeof year); // will be undefined because no data type and no value has been given
+    year = 1991;
+    console.log(typeof year) //will now be number
+
+    console.log(typeof null) //this is an error in javascript with using typeof on null, which returns an "object" although it shoudld actually be "null". This bug has not been fixed in javascript for legacy reasons.
+
+*/
+
+//PART 3 - Using Final in java
+
+/*
+const birthYear = 1991; // Const variable which is basically a final variblae in java (It cannot change it's value)
+
+var job = "programmer"; // VAR IS SOMETHING IN LEGACY CODE OF JAVSCRIPT, AVOID USING IT. IT IS USED TO DECLARE VARIABLES
+job = "teacher"; // VAR IS SOMETHING IN LEGACY CODE OF JAVSCRIPT, AVOID USING IT. IT IS USED TO DECLARE VARIABLES
+
+lastName = "Schmdetmann" // This is another way to declare a variable, but is bad way of doing so, Always use "let before declaring."
+*/
